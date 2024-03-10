57ca9958b90f6df0aa13c6b44cf42e64ce0353c8
Eduard <sorokolietov@gmail.com>
2024-03-10 11:42:23 +0100

    add: assets, imgs, svgs

---
+++ src/assets/makes.js
@@ -0,0 +1,90 @@
+export default [
+  {
+    value:"123",
+    label: "Buick",
+  },
+  {
+    value:"Volvo",
+    label: "Volvo",
+  },
+  {
+    value:"HUMMER",
+    label: "HUMMER",
+  },
+  {
+    value:"Subaru",
+    label: "Subaru",
+  },
+  {
+    value:"Mitsubishi",
+    label: "Mitsubishi",
+  },
+  {
+    value:"Nissan",
+    label: "Nissan",
+  },
+  {
+    value:"Lincoln",
+    label: "Lincoln",
+  },
+  {
+    value:"GMC",
+    label: "GMC",
+  },
+  {
+    value:"Hyundai",
+    label: "Hyundai",
+  },
+  {
+    value:"MINI",
+    label: "MINI",
+  },
+  {
+    value:"Bentley",
+    label: "Bentley",
+  },
+  {
+    value:"Mercedes-Benz",
+    label: "Mercedes-Benz",
+  },
+  {
+    value:"Aston Martin",
+    label: "Aston Martin",
+  },
+  {
+    value:"Pontiac",
+    label: "Pontiac",
+  },
+  {
+    value:"Lamborghini",
+    label: "Lamborghini",
+  },
+  {
+    value:"Audi",
+    label: "Audi",
+  },
+  {
+    value:"BMW",
+    label: "BMW",
+  },
+  {
+    value:"Chevrolet",
+    label: "Chevrolet",
+  },
+  {
+    value:"Mercedes-Benz",
+    label: "Mercedes-Benz",
+  },
+  {
+    value:"Chrysler",
+    label: "Chrysler",
+  },
+  {
+    value:"Kia",
+    label: "Kia",
+  },
+  {
+    value:"Land",
+    label: "Land",
+  },
+]
