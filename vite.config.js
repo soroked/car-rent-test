b2b6facdc8326feb831d2f2be564a23b56ab6e18
Eduard <sorokolietov@gmail.com>
2024-03-10 11:41:17 +0100

    config absolute Imports

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/car-rent-test/",
+  // base: "/car-rent-test/",
+  resolve: {
+    alias: {
+      src: "/src",
+      pages: "src/pages",
+      components: "/src/components",
+      assets: "/src/assets",
+    },
+  },
 })
