f34d1d652f258c068a7afdbe4cc377a84c97a1c1
Eduard <sorokolietov@gmail.com>
2024-03-10 11:43:44 +0100

    add: Favorite Page

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import FilterForm from "src/components/FilterForm/FilterForm";
+import css from "src/components/CarCards/CarCards.module.css";
+import defaultImg from "../assets/defaultCarImg.jpg";
+import HeartSvg from "src/assets/HeartSvg";
+import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "src/redux/modal/modal.reducer";
+import Modal from "src/components/Modal/Modal";
+
+const FavoritesPage = () => {
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favoriteCars")) ?? []
+  );
+  const isOpenModal = useSelector((state) => state.modal.isOpenModal);
+
+  const dispatch = useDispatch();
+
+  const toggleFavorite = (car) => {
+    const favoriteCars = JSON.parse(localStorage.getItem("favoriteCars")) ?? [];
+
+    const isFavoriteCar =
+      favoriteCars.length > 0
+        ? favoriteCars.find((favorite) => favorite.id === car.id)
+        : false;
+
+    if (isFavoriteCar) {
+      const newFavoriteCars = favoriteCars.filter(
+        (favorite) => car.id !== favorite.id
+      );
+      localStorage.setItem("favoriteCars", JSON.stringify(newFavoriteCars));
+      setFavorites(newFavoriteCars);
+    } else {
+      localStorage.setItem(
+        "favoriteCars",
+        JSON.stringify([...favoriteCars, car])
+      );
+      setFavorites([...favoriteCars, car]);
+    }
+
+    // clean local storage
+    if (JSON.parse(localStorage.getItem("favoriteCars")).length === 0) {
+      localStorage.removeItem("favoriteCars");
+    }
+  };
+
+  const isCarFavorite = (id) => {
+    const isFavorite = favorites.find((car) => car.id === id);
+    return isFavorite ? "#3470ff" : null;
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          paddingLeft: "15px",
+          paddingRight: "15px",
+        }}
+      >
+        <FilterForm />
+        <ul className={css.carCardsContainer} style={{}}>
+          {favorites?.map((car) => (
+            <li key={car.id}>
+              <div className={css.carCardImgContainer}>
+                <img
+                  className={css.carCardImage}
+                  src={car.img ?? defaultImg}
+                  alt={car.make}
+                />
+                <HeartSvg
+                  onClick={() => {
+                    toggleFavorite(car);
+                  }}
+                  fillColor={isCarFavorite(car.id)}
+                />
+              </div>
+              <div>
+                <div className={css.carCardHeader}>
+                  <p className={css.carCardHeaderName}>
+                    {car.make}{" "}
+                    <span className={css.secondColor}>{car.model}</span>,{" "}
+                    {car.year}
+                  </p>
+                  <p className={css.carCardHeaderPrice}>{car.rentalPrice}</p>
+                </div>
+                <div className={css.carCardInfo}>
+                  <span>{car.address.split(",")[1]}</span>
+                  <span>{car.address.split(",")[2]}</span>
+                  <span>{car.rentalCompany}</span>
+                  <span>{car.type}</span>
+                  <span>{car.model}</span>
+                  <span>{car.id}</span>
+                  <span>{car.accessories[0]}</span>
+                </div>
+              </div>
+              <button
+                className={css.carCardButton}
+                onClick={() => dispatch(openModal(car))}
+              >
+                Learn more
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {isOpenModal && <Modal />}
+    </>
+  );
+};
+
+export default FavoritesPage;
