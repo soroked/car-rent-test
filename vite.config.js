3f0c0384292af4b7e323aabcbf96633befc7d1de
Eduard <sorokolietov@gmail.com>
2024-03-10 11:49:36 +0100

    vite-config: base-url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/car-rent-test/",
+  base: "/car-rent-test/",
   resolve: {
     alias: {
       src: "/src",
